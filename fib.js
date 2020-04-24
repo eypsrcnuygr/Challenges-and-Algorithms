99461e780f94d0a77f342e7d7daf193d9bc127e2
eypsrcnuygr <sercanuygur@gmail.com>
2020-04-24 23:29:11 +0300

    Fibonachi is finished

--- fib.js
+++ fib.js
@@ -3,4 +3,27 @@ function fib(n){
     return fib(n-1) + fib(n-2);
 }
 
-console.log(fib(15));
+
+function fibWithMemoization (n, memo = []) {
+    if (n <= 2) {
+        return 1
+    }
+    if( memo[n] !== undefined) {
+        return memo[n]
+    }
+    var result = fibWithMemoization(n - 1, memo) + fibWithMemoization(n - 2, memo);
+    memo[n] = result;
+    return result;
+}
+
+
+function fibWithTabulation(n) {
+    if (n <= 2) return 1;
+    var fibNums = [0 ,1, 1];
+    for (let i = 3; i <= n; i++) {
+        fibNums[i] = fibNums[i - 1] + fibNums[i - 2];
+    }
+    return fibNums[n]
+}
+
+console.log(fibWithTabulation(40))
